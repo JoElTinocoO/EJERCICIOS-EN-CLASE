d1efc895a6011899cad49512ccb2cb2fd9c1cf6b
JoElTinocoO <151596202+JoElTinocoO@users.noreply.github.com>
2024-09-21 10:52:27 -0700

    Add files via upload

---
+++ area triangulo.py
@@ -0,0 +1,6 @@
+base = float(input("Ingresa la base del triángulo: "))
+altura = float(input("Ingresa la altura del triángulo: "))
+
+area = (base * altura) / 2
+
+print(f"área del triángulo correcta: {area:.2f} unidades cuadradas.")
